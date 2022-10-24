cd19a8055d33437e66b6d665916b71e5c2ac0933
Adrián González Galván <alu0101321219@ull.edu.es>
2022-10-24 13:27:16 +0000

    Hacemos expr regular para los comentarios

--- cya-p4_expresiones_regulares/codeAnalyser.cc
+++ cya-p4_expresiones_regulares/codeAnalyser.cc
@@ -21,20 +21,31 @@ CodeAnalyser::CodeAnalyser(std::istream& is) {
 
   while (!is.eof()) {
     getline(is, reader);
-    std::cout << ">> " << reader << std::endl;
-
-    if (!isVariable(reader).empty()) { // Es una variable
-      std::vector<std::string> result = isVariable(reader);
-      Variable var(result[0], result[1], result[2]);
-      variables_.insert(std::pair<int, Variable>(line, var));
-    } else if (!isComment(reader).empty()) { // Es un comentario
-      comments_.insert(std::pair<int, std::string>(line, isComment(reader)));
-    } else if (!isLoop(reader).empty()) { // Es un bucle
-      loops_.insert(std::pair<int, std::string>(line, isLoop(reader)));
+    //std::cout << ">> " << reader << std::endl;
+    if (isMultiLineComment(reader)) {
+      int startLine = line;
+      std::string content = reader + "\n";
+      do {
+        getline(is, reader);
+        content += reader + "\n";
+        line++;
+      } while (!bool(std::regex_match(reader, kMultiCommentCloseRE)));
+      comments_.insert(std::pair<std::pair<int, int>,
+        std::string>(std::pair<int, int>(startLine, line), content));
+    } else if (isVariable(reader)) { // Es una variable
+      std::smatch m;
+      std::regex_search(reader, m, kVariableRE);
+      variables_.insert(std::pair<int, Variable>(line, Variable(m[1], m[2], m[4])));
+    } else if (isComment(reader)) { // Es un comentario
+      comments_.insert(std::pair<std::pair<int, int>,
+        std::string>(std::pair<int, int>(line, 0), reader));
+    } else if (isLoop(reader)) { // Es un bucle
+      std::smatch m;
+      std::regex_search(reader, m, kLoopRE);
+      loops_.insert(std::pair<int, std::string>(line, m[1]));
     } else if(isMain(reader)) { // Es la declaración del  main
       main_ = true;
     }
-      
     line++;
   }
 
@@ -43,65 +54,52 @@ CodeAnalyser::CodeAnalyser(std::istream& is) {
 // Destructor
 CodeAnalyser::~CodeAnalyser() {}
 
+// Getters
+std::string
+CodeAnalyser::getDescription() {
+  for (auto element: comments_) {
+    if (element.first.first == 1 && element.first.second != 0)
+      return element.second;
+  }
+  return "";
+}
 
 // Comprueba si una línea es el comienzo de una descripción
-//bool
-//CodeAnalyser::isDescription(std::string str) {
-//  std::regex re("\\/\\")
-//}
+bool
+CodeAnalyser::isMultiLineComment(std::string str) {
+  return bool(std::regex_match(str, kMultiCommentRE));
+}
 
 // Comprueba si una línea de código es la declaración de una variable
-std::vector<std::string>
+bool
 CodeAnalyser::isVariable(std::string str) {
-  std::vector<std::string> result;
-  std::regex re("\\s*(int|double)\\s([a-zA-Z]+\\d*)+(\\s=\\s(.*))?;$");
-  std::smatch m;
-  if (std::regex_match(str, re)) {
-    std::smatch m;
-    std::regex_search(str, m, re);
-    result.push_back(m[1]); // type
-    result.push_back(m[2]); // name
-    result.push_back(m[4]); // value
-  }
-  return result;
+  return bool(std::regex_match(str, kVariableRE));
 }
 
 // Comprueba si una línea de código es un comentario
-std::string
+bool
 CodeAnalyser::isComment(std::string str) {
-  std::string result = "";
-  std::regex re("\\s*\\/\\/.*");
-  if (std::regex_match(str, re)) {
-    result = str;
-  }
-  return result;
+  return bool(std::regex_match(str, kSingleCommentRE));
 }
 
 // Comprueba si una línea de código es la declaración de un bucle
-std::string
+bool
 CodeAnalyser::isLoop(std::string str) {
-  std::string result = "";
-  std::regex re("\\s*(for|while)\\s\\(.*\\)\\s\\{$");
-  std::smatch m;
-  if (std::regex_match(str, re)) {
-    std::smatch m;
-    std::regex_search(str, m, re);
-    result = m[1];
-  }
-  return result;
+  return bool(std::regex_match(str, kLoopRE));
 }
 
 // Comprueba si una línea de código es la función del main
 bool
 CodeAnalyser::isMain(std::string str) {
-  std::regex re("^int\\smain\\s\\(.*\\)\\s\\{$");
-  return bool(std::regex_match(str, re));
+  return bool(std::regex_match(str, kMainRE));
 }
 
 // Escritura
 void
 CodeAnalyser::write(std::ostream& os) {
-  os << "VARIABLES:\n";
+  os << "DESCRIPTION:\n";
+  os << getDescription();
+  os << "\nVARIABLES:\n";
   for (auto element: variables_) {
     os << "[Line " << element.first << "] " << element.second << "\n"; 
   }
@@ -116,6 +114,10 @@ CodeAnalyser::write(std::ostream& os) {
     os << "False\n";
   os << "\nCOMMENTS:\n";
   for (auto element: comments_) {
-    os << "[Line " << element.first << "] " << element.second << "\n";
+    if (element.first.second == 0) {
+      os << "[Line " << element.first.first << "] " << element.second << "\n";
+    } else {
+      os << "[Line " << element.first.first << "-" << element.first.second << "] DESCRIPTION\n";
+    }
   }
 }
--- cya-p4_expresiones_regulares/codeAnalyser.h
+++ cya-p4_expresiones_regulares/codeAnalyser.h
@@ -20,24 +20,32 @@
 
 #include "variable.h"
 
+const std::regex kVariableRE("\\s*(int|double)\\s([a-zA-Z]+\\d*)+(\\s=\\s(.*))?;$");
+const std::regex kSingleCommentRE("\\s*\\/\\/.*");
+const std::regex kMultiCommentRE("\\/\\*.*");
+const std::regex kMultiCommentCloseRE(".*\\*\\/");
+const std::regex kLoopRE("\\s*(for|while)\\s\\(.*\\)\\s\\{$");
+const std::regex kMainRE("^int\\smain\\s\\(.*\\)\\s\\{$");
+
 class CodeAnalyser {
 
   public:
     CodeAnalyser(std::istream&);
     ~CodeAnalyser();
 
+    std::string getDescription();
+
     void write(std::ostream& = std::cout);
   
   private:
-    //std::string description_;
-    std::set<std::pair<int, std::string>> comments_;
+    std::set<std::pair<std::pair<int, int>, std::string>> comments_;
     std::set<std::pair<int, Variable>> variables_;
     std::set<std::pair<int, std::string>> loops_;
     bool main_ = false;
 
-    //bool isDescription(std::string);
-    std::vector<std::string> isVariable(std::string);
-    std::string isComment(std::string);
-    std::string isLoop(std::string);
+    bool isMultiLineComment(std::string);
+    bool isVariable(std::string);
+    bool isComment(std::string);
+    bool isLoop(std::string);
     bool isMain(std::string);
 };
